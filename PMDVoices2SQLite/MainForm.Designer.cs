1677dcceaab2b604a67c5fb8a68477008ee4f5f8
average34 <dreamy.phantasm@gmail.com>
2022-02-08 22:38:42 +0900

    PMDVoiceレコード構造体を作成

--- PMDVoices2SQLite/MainForm.Designer.cs
+++ PMDVoices2SQLite/MainForm.Designer.cs
@@ -28,12 +28,97 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.ファイルToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mMLファイルを開くOToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mMLファイルを保存SToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.終了XToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.statusStrip1.Location = new System.Drawing.Point(0, 428);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(800, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ファイルToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 28);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // ファイルToolStripMenuItem
+            // 
+            this.ファイルToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mMLファイルを開くOToolStripMenuItem,
+            this.mMLファイルを保存SToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.終了XToolStripMenuItem});
+            this.ファイルToolStripMenuItem.Name = "ファイルToolStripMenuItem";
+            this.ファイルToolStripMenuItem.Size = new System.Drawing.Size(82, 24);
+            this.ファイルToolStripMenuItem.Text = "ファイル(&F)";
+            // 
+            // mMLファイルを開くOToolStripMenuItem
+            // 
+            this.mMLファイルを開くOToolStripMenuItem.Name = "mMLファイルを開くOToolStripMenuItem";
+            this.mMLファイルを開くOToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
+            this.mMLファイルを開くOToolStripMenuItem.Size = new System.Drawing.Size(276, 26);
+            this.mMLファイルを開くOToolStripMenuItem.Text = "MMLファイルを開く(&O)";
+            this.mMLファイルを開くOToolStripMenuItem.Click += new System.EventHandler(this.mMLファイルを開くOToolStripMenuItem_Click);
+            // 
+            // mMLファイルを保存SToolStripMenuItem
+            // 
+            this.mMLファイルを保存SToolStripMenuItem.Name = "mMLファイルを保存SToolStripMenuItem";
+            this.mMLファイルを保存SToolStripMenuItem.Size = new System.Drawing.Size(276, 26);
+            this.mMLファイルを保存SToolStripMenuItem.Text = "MMLファイルを保存(&S)";
+            // 
+            // 終了XToolStripMenuItem
+            // 
+            this.終了XToolStripMenuItem.Name = "終了XToolStripMenuItem";
+            this.終了XToolStripMenuItem.Size = new System.Drawing.Size(276, 26);
+            this.終了XToolStripMenuItem.Text = "終了(&X)";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(273, 6);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "MainForm";
+            this.Text = "PMDVoices2SQLite";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private StatusStrip statusStrip1;
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem ファイルToolStripMenuItem;
+        private ToolStripMenuItem mMLファイルを開くOToolStripMenuItem;
+        private ToolStripMenuItem mMLファイルを保存SToolStripMenuItem;
+        private ToolStripSeparator toolStripSeparator1;
+        private ToolStripMenuItem 終了XToolStripMenuItem;
     }
 }
--- PMDVoices2SQLite/MainForm.cs
+++ PMDVoices2SQLite/MainForm.cs
@@ -6,5 +6,11 @@ namespace PMDVoices2SQLite
         {
             InitializeComponent();
         }
+
+        private void mMLファイルを開くOToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
     }
 }
